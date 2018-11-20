51ed084f5016240ce625b45633af206e259c6c8c
sirrah23 <hseth93@gmail.com>
2018-11-20 17:12:36 -0500

    Add additional tests

--- server/test/test.js
+++ server/test/test.js
@@ -1,7 +1,7 @@
 process.env.NODE_ENV = 'test'
 
 let mongoose = require("mongoose")
-let suggestionBox = require('../model/suggestionBox.js')
+let suggestionBoxRepo = require('../repo/suggestionBox.js')
 
 let chai = require('chai')
 let chaiHttp = require('chai-http')
@@ -12,14 +12,11 @@ chai.use(chaiHttp)
 
 describe('Suggestion Box', () => {
 
-    beforeEach((done) => {
-        suggestionBox.remove({}, (err) => { 
-            done()
-        })
-    })
+    beforeEach(() => { suggestionBoxRepo.clear() })
 
-  describe('/GET suggestion box', () => {
-    it('should GET all of the suggestion boxes', (done) => {
+  describe('/GET suggestion boxes', () => {
+
+    it('should GET all of the suggestion boxes when there are none', (done) => {
         chai.request(server)
             .get('/suggestionbox')
             .end((err, res) => {
@@ -27,9 +24,57 @@ describe('Suggestion Box', () => {
                 res.body.should.be.a('array')
                 res.body.length.should.be.eql(0)
                 done()
-        }) })
-})
+            }) 
+        })
 
+    it('should GET all of the suggestion boxes when there is one', (done) => {
+        const box = suggestionBoxRepo.createBox({name: "New menu items for cafeteria"})
+        chai.request(server)
+            .get('/suggestionbox')
+            .end((err, res) => {
+                res.should.have.status(200)
+                res.body.should.be.a('array')
+                res.body.length.should.be.eql(1)
+                done()
+            }) 
+        })
+    })
+
+    describe('/GET a specific suggestion box', () => {
+        const box = suggestionBoxRepo.createBox({name: "New menu items for cafeteria"})
+
+        it('should GET an existing suggestion box by owner hash', (done) => {
+            chai.request(server)
+                .get(`/suggestionbox/${box.hash_owner}`)
+                .end((err , res) => {
+                    res.should.have.status(200)
+                    res.body.should.be.a('object')
+                    res.body.should.not.have.property('error')
+                    res.body.should.have.property('name').eql(box.name)
+                    res.body.should.have.property('date')
+                    res.body.should.have.property('hash_owner').eql(box.hash_owner)
+                    res.body.should.have.property('hash_submitter').eql(box.hash_submitter)
+                    res.body.should.have.property('suggestions').length.should.be.eql(0)
+                    done()
+                })
+        })
+
+        it('should GET an existing suggestion box by submitter hash', (done) => {
+            chai.request(server)
+                .get(`/suggestionbox/${box.hash_submitter}`)
+                .end((err , res) => {
+                    res.should.have.status(200)
+                    res.body.should.be.a('object')
+                    res.body.should.not.have.property('error')
+                    res.body.should.have.property('name').eql(box.name)
+                    res.body.should.have.property('date')
+                    res.body.should.have.property('hash_owner').eql(box.hash_owner)
+                    res.body.should.have.property('hash_submitter').eql(box.hash_submitter)
+                    res.body.should.have.property('suggestions').length.should.be.eql(0)
+                    done()
+                })
+        })
+    })
 
     describe('/POST suggestionBox', () => {
         it('should POST a suggestionBox without suggestions', (done) => {
@@ -49,7 +94,29 @@ describe('Suggestion Box', () => {
                     res.body.should.have.property('hash_submitter')
                     res.body.should.have.property('suggestions')
                     done()
-            })
+                })
+        })
+    })
+
+    describe('/POST suggestion to box', () => {
+        it('should POST a suggestion to an exisiting box', (done) => {
+            const box = suggestionBoxRepo.createBox({name: 'Boost morale'})
+            const suggestion= {
+                body: 'Lets have a party!'
+            }
+            chai.request(server)
+                .post(`/suggestionbox/${box.hash_submitter}`)
+                .send(suggestion)
+                .end((err, res) => {
+                    res.should.have.status(200)
+                    res.should.not.have.property('error')
+                    res.should.be.a('object')
+                    res.body.should.have.property('name')
+                    res.body.should.have.property('date')
+                    res.body.should.have.property('hash_owner')
+                    res.body.should.have.property('hash_submitter')
+                    res.body.should.have.property('suggestions')
+                })
         })
     })
 })
