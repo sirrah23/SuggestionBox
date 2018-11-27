707c2d05a5f1877b7d0f97fa07e123a2606c3930
sirrah23 <hseth93@gmail.com>
2018-11-26 19:24:57 -0500

    Show user links to newly created rooms

---
+++ frontend/src/components/BoxLinks.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="links">
+      <div class="collection">
+        <a :href="ownerLink" class="collection-item">Room for you to review suggestions</a>
+        <a :href="submitterLink" class="collection-item">Room for people to submit suggestions</a>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    boxData: {
+      type: Object
+    }
+  },
+  computed: {
+    ownerLink() {
+      return `http://localhost:3000/room_own/${this.boxData.hash_owner}`;
+    },
+    submitterLink() {
+      return `http://localhost:3000/room_sub/${this.boxData.hash_submitter}`;
+    }
+  }
+};
+</script>
+
+<style>
+.links {
+  max-width: 600px;
+  margin-top: 100px;
+}
+</style>
--- frontend/src/components/Welcome.vue
+++ frontend/src/components/Welcome.vue
@@ -3,17 +3,20 @@
     <div class="card">
       <div class="card-content center-align">
         <h2 class="teal-text">Suggestion Box</h2>
-        <form @submit.prevent="createBox">
+        <p v-if="this.errorMsg" class="red-text">{{this.errorMsg}}</p>
+        <form v-if="!submitted" @submit.prevent="createBox">
           <label for="name">Enter your box name:</label>
           <input type="text" name="name" v-model="name">
           <button class="btn teal">Create</button>
         </form>
+        <BoxLinks v-if="submitted" :boxData="boxData"/>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import BoxLinks from "@/components/BoxLinks.vue";
 import SuggestionBoxAPI from "@/utils/api.js";
 
 const sboxapi = new SuggestionBoxAPI("http://localhost:3000");
@@ -21,16 +24,30 @@ const sboxapi = new SuggestionBoxAPI("http://localhost:3000");
 export default {
   data() {
     return {
-      name: null
+      name: null,
+      boxData: null,
+      errorMsg: null,
+      submitted: false
     };
   },
   methods: {
     createBox() {
       sboxapi
-        .createBox(`Henlo ${this.name}`)
-        .then(res => console.log(res))
+        .createBox(this.name)
+        .then(res => {
+          if (res.error) {
+            console.log(res.error);
+            this.errorMsg = "Something went wrong...";
+          } else {
+            this.boxData = res.data;
+            this.submitted = true;
+          }
+        })
         .catch(err => console.log(err));
     }
+  },
+  components: {
+    BoxLinks
   }
 };
 </script>
